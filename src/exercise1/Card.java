a7dd7a5dc7da6c21b7681a41ea65e8a24e3a28df
Maninderpal Singh <sin14329@sheridancollege.ca>
2024-02-06 11:53:21 -0500

    Author Changed

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -7,8 +7,8 @@ package exercise1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
- * @author Paul Bonenfant May 2020
+ * @author Maninderpal Singh
+ * @author Maninderpal Singh Feb 2024
  */
 public class Card {
 
